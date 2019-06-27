49a838817cc9b13be0864b8c9affaf5f65c1e9d7
Edison Duong <edisonvduong@gmail.com>
2019-06-27 17:30:10 -0500

    fixed decimal places for total

--- src/components/Transportation.js
+++ src/components/Transportation.js
@@ -291,7 +291,7 @@ class Transportation extends Component {
                     <div className="orderDetail">Subtotal: <strong>${this.state.pizzaCost.toFixed(2)}</strong></div>
                     <div className="orderDetail">Delivery cost: <strong>${this.state.deliveryCost.toFixed(2)}</strong></div>
                     <div className="orderDetail">Tax: <strong>${this.state.tax}</strong></div>
-                    <div className="orderDetail">Total: <strong>${this.state.totalCost * this.state.tax}</strong></div>
+                    <div className="orderDetail">Total: <strong>${(this.state.totalCost * 1.07).toFixed(2)}</strong></div>
 
                     <div className="orderDetail">Pizza Size: <strong>{this.state.pizzaSize}</strong></div>
                     <div className="orderDetail">Pizza Crust: <strong>{this.state.pizzaCrust}</strong></div>
